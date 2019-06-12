97e3e160c7e2e81254a79dceabb1d1c70515d669
Aki Rehn <aki.rehn@helsinki.fi>
2019-06-12 17:44:57 +0300

    Commit forgotten files for part2.

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/persons'
+
+const getAll = () => axios.get(baseURL).then(response => response.data)
+const create = (entry) => axios.post(baseURL, entry).then(response => response.data)
+const update = (entry) => axios.put(`${baseURL}/${entry.id}`, entry).then(response => response.data)
+const remove = (id) => axios.delete(`${baseURL}/${id}`)
+const get = (id) => axios.get(`${baseURL}/${id}`).then(response => response.data)
+
+export default { getAll, create, update, remove, get }
