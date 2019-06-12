7d1590b25f423f61910d6f098c484bc290c08af5
Aki Rehn <aki.rehn@helsinki.fi>
2019-06-12 18:04:09 +0300

    Add frontend.

---
+++ part3/phonebook-backend/build/precache-manifest.2e6da0c35b6eaacc05bb71049431c4ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffd93691b722622796aef5619471ae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d538652694bd439d49af",
+    "url": "/static/js/2.f2f882f0.chunk.js"
+  },
+  {
+    "revision": "9b7627b3ad1ad8aa4772",
+    "url": "/static/js/main.67a71a7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
